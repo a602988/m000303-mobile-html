a8cc05c8326fb81e8bbbc43f218904e805946304
jean <a680603@gmail.com>
2020-07-30 14:02:32 +0800

    test

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,7 +12,6 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 /* Plugins */
 
 import nuxt_plugin_plugin_4da65357 from 'nuxt_plugin_plugin_4da65357' // Source: ./components/plugin.js (mode: 'all')
-import nuxt_plugin_plugin_71fe87f7 from 'nuxt_plugin_plugin_71fe87f7' // Source: ./vuetify/plugin.js (mode: 'all')
 import nuxt_plugin_pluginrouting_3d1946b9 from 'nuxt_plugin_pluginrouting_3d1946b9' // Source: ./nuxt-i18n/plugin.routing.js (mode: 'all')
 import nuxt_plugin_pluginmain_3c727154 from 'nuxt_plugin_pluginmain_3c727154' // Source: ./nuxt-i18n/plugin.main.js (mode: 'all')
 
@@ -53,7 +52,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"titleTemplate":"%s - m000303-moblie-html","title":"m000303-moblie-html","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"## Build Setup"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"},{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:100,300,400,500,700,900&display=swap"},{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Fcdn.jsdelivr.net\u002Fnpm\u002F@mdi\u002Ffont@latest\u002Fcss\u002Fmaterialdesignicons.min.css"}],"style":[],"script":[]},
+    head: {"titleTemplate":"%s - m000303-moblie-html","title":"m000303-moblie-html","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"## Build Setup"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
     router,
     nuxt: {
@@ -169,10 +168,6 @@ async function createApp(ssrContext, config = {}) {
 
   if (typeof nuxt_plugin_plugin_4da65357 === 'function') {
     await nuxt_plugin_plugin_4da65357(app.context, inject)
-  }
-
-  if (typeof nuxt_plugin_plugin_71fe87f7 === 'function') {
-    await nuxt_plugin_plugin_71fe87f7(app.context, inject)
   }
 
   if (typeof nuxt_plugin_pluginrouting_3d1946b9 === 'function') {
